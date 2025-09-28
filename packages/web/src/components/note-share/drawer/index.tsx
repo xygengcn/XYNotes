6b239882b42d1969eb5cc63f5a2d200564087199
xygengcn <xygengcn@qq.com>
2025-09-28 18:48:25 +0800

    feat(分享): 修改移动端分享图片下载和复制的弹窗

---
+++ packages/web/src/components/note-share/drawer/index.tsx
@@ -0,0 +1,92 @@
+import { Drawer, Icon, Loading } from '@xynotes/components';
+import { EditorPerview, useEditorPreview } from '@xynotes/editor';
+import type { Note } from '@xynotes/store';
+import { screenshot, screenshotCopy } from '@xynotes/utils';
+import { defineComponent, ref, type PropType } from 'vue';
+import './index.scss';
+export default defineComponent({
+  name: 'NoteShareDrawer',
+  props: {
+    note: {
+      type: Object as PropType<Note>,
+      required: true
+    }
+  },
+  emits: ['close'],
+  setup(props, context) {
+    /**
+     * 加载
+     */
+    const loading = ref(false);
+
+    /**
+     * 预览
+     */
+    const { previewElement } = useEditorPreview();
+
+    /**
+     * 下载
+     */
+    const handleClickDownalodScreenshot = () => {
+      loading.value = true;
+      const el = previewElement();
+      // 生成截图
+      return screenshot(el, props.note.title)
+        .catch((e) => {
+          console.error('[screenshot]', e);
+          window.$ui.toast('生成图片失败');
+        })
+        .finally(() => {
+          loading.value = false;
+        });
+    };
+
+    /**
+     * 复制图片
+     * @returns
+     */
+    const handleClickCopyImage = () => {
+      loading.value = true;
+      const el = previewElement();
+      // 生成截图
+      return screenshotCopy(el)
+        .then(() => {
+          window.$ui.toast('复制图片成功');
+        })
+        .catch((e) => {
+          console.error('[screenshotCopy]', e);
+          window.$ui.toast('复制图片失败');
+        })
+        .finally(() => {
+          loading.value = false;
+        });
+    };
+
+    /**
+     * 隐藏
+     */
+    const hide = () => {
+      context.emit('close');
+    };
+    return () => (
+      <Drawer visible={true} onClose={hide}>
+        <div class="note-share-drawer">
+          <div class="note-share-drawer-header">
+            <Icon type="item-copy" size={20} onClick={handleClickCopyImage}></Icon>
+            <Icon type="item-preview" size={20} onClick={handleClickDownalodScreenshot}></Icon>
+          </div>
+          <div class="note-share-drawer-content">
+            <div class="note-share-drawer-content-preview">
+              <EditorPerview value={props.note.content || props.note.text || ''} />
+            </div>
+            {loading.value && (
+              <div class="note-share-drawer-content-loading">
+                <Loading text="加载中" />
+              </div>
+            )}
+          </div>
+        </div>
+      </Drawer>
+    );
+  }
+});
