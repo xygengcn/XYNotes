9e238f0994e6b26ff30f1eca30819f23f8245f38
xygengcn <xygengcn@qq.com>
2025-10-13 21:37:03 +0800

    feat(note): 修改标签保存机制

--- packages/web/src/services/note-tags/drawer/index.tsx
+++ packages/web/src/services/note-tags/drawer/index.tsx
@@ -1,6 +1,6 @@
-import type { Note } from '@xynotes/store';
+import { ApiEvent, Note } from '@xynotes/store';
 import { NoteStatus } from '@xynotes/typings';
-import { defineComponent, type PropType } from 'vue';
+import { defineComponent, ref, type PropType } from 'vue';
 import NoteTagsContent from '../content';
 import './index.scss';
 
@@ -13,6 +13,12 @@ export default defineComponent({
     }
   },
   setup(props, context) {
+    const note = ref<Note>();
+
+    // 获取最新的笔记数据
+    ApiEvent.api.apiFetchNoteDetailData(props.note.nid).then((data) => {
+      note.value = new Note(data);
+    });
     const handleClose = () => {
       context.emit('close');
     };
@@ -21,7 +27,8 @@ export default defineComponent({
      * 保存
      */
     const handleClickSubmit = () => {
-      props.note.update({ tags: props.note.tags, updatedAt: Date.now(), status: NoteStatus.draft });
+      note.value.set({ tags: note.value.tags, status: NoteStatus.draft });
+      note.value.save(true);
       handleClose();
     };
     return () => (
@@ -35,7 +42,7 @@ export default defineComponent({
             确定
           </span>
         </div>
-        <NoteTagsContent note={props.note}></NoteTagsContent>
+        <NoteTagsContent note={note.value}></NoteTagsContent>
       </div>
     );
   }
